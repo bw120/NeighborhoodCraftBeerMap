208fb2262f505b51274ac711c2f8b3e93e5f9eb4
bw120 <benwright120@yahoo.com>
2015-08-28 15:54:10 -0400

    Feat: Added function to change map bounds based on data from AJAX/search

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@
 var map;
 var theLocation = "42.3600825, -71.05888010000001";
 
+var bounds;
+
 var initMap = function() {
 
-  var bounds = new google.maps.LatLng(42.3600825, -71.05888010000001);
+  bounds = new google.maps.LatLng(42.3600825, -71.05888010000001);
   var mapOptions = {
     zoom: 13,
     center: bounds,
@@ -20,51 +22,8 @@ var initMap = function() {
     }
   }
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
-};
-
-
-///get brewery data from Foursquare
-var getBreweryInfo = function() {
-  var self = this;
-  self.breweryInfo = ko.observableArray();
-  self.brewDataFailed = ko.observable(true);
-  self.faultReason = ko.observable("Loading data...");
-
-
-  //set up URL for Ajax request
-  var categoryID = "50327c8591d4c4b30a586d5d";
-  var radius = "100000";
-  var theURL = "https://api.foursquare.com/v2/venues/search?client_id=V5XZKQRSRXGVGCGN5U3YIFCXTIAZWCZA01V3U5ICI4KRNXOX&client_secret=0ATJHEHUGP41GLOHJJS4ACJC3ENKG311BRW2KR510Y2FPSPY&v=20130815&ll=" + theLocation + " &categoryId="+ categoryID + " &intent=browse&radius=" + radius;
-
-  //send AJAX request
-  $.getJSON(theURL, function(data){
-      var breweryData = data.response.venues;
-      var brewArr = [];
-
-      //Foursquare API search by categoryID still gives a few venues not in that category. 
-      //So here we have to remove what we don't want.
-      for (i = 0; i < breweryData.length; i++) {
-        if (breweryData[i].categories[0].id === categoryID) {
-          brewArr.push(breweryData[i]);
-        }        
-      }
-
-      //confirm the ajax search brough up results and set variable to show ajax request was successful 
-      //and post results to obserable. Otherwise log fault.
-      if (brewArr.length > 0) {
-        self.brewDataFailed(false);
-        self.breweryInfo(brewArr);   
-      } else {
-        self.faultReason("Sorry, we were not able to find any breweries");
-      }
-      
-
-  }).error(function(e) {
-    self.faultReason("Sorry, we were not able to load the data");
-    console.log("There was an error getting brewery info")});
 
 };
-
 
 //setup markers on map
 var setUpMarkers = function(data, failed) {
@@ -111,6 +70,64 @@ var setUpInfoWindows = function(data, markers, failed) {
       }    
     }
 };
+//set bounds of map based on data returned from ajax/search
+var setBounds = function(data) {
+  var mapBounds = new google.maps.LatLngBounds();
+  var lat;
+  var lng;
+
+  for (var i = 0; i < data.length; i++) {
+    lat = data[i].location.lat;
+    lng = data[i].location.lng;
+    mapBounds.extend(new google.maps.LatLng(lat, lng));
+  }
+  map.fitBounds(mapBounds);
+  map.setCenter(mapBounds.getCenter());
+}
+
+
+///get brewery data from Foursquare
+var getBreweryInfo = function() {
+  var self = this;
+  self.breweryInfo = ko.observableArray();
+  self.brewDataFailed = ko.observable(true);
+  self.faultReason = ko.observable("Loading data...");
+
+
+  //set up URL for Ajax request
+  var categoryID = "50327c8591d4c4b30a586d5d";
+  var radius = "100000";
+  var theURL = "https://api.foursquare.com/v2/venues/search?client_id=V5XZKQRSRXGVGCGN5U3YIFCXTIAZWCZA01V3U5ICI4KRNXOX&client_secret=0ATJHEHUGP41GLOHJJS4ACJC3ENKG311BRW2KR510Y2FPSPY&v=20130815&ll=" + theLocation + " &categoryId="+ categoryID + " &intent=browse&radius=" + radius;
+
+  //send AJAX request
+  $.getJSON(theURL, function(data){
+      var breweryData = data.response.venues;
+      var brewArr = [];
+
+      //Foursquare API search by categoryID still gives a few venues not in that category. 
+      //So here we have to remove what we don't want.
+      for (i = 0; i < breweryData.length; i++) {
+        if (breweryData[i].categories[0].id === categoryID) {
+          brewArr.push(breweryData[i]);
+        }        
+      }
+
+      //confirm the ajax search brough up results and set variable to show ajax request was successful 
+      //and post results to obserable. Otherwise log fault.
+      if (brewArr.length > 0) {
+        self.brewDataFailed(false);
+        self.breweryInfo(brewArr);   
+      } else {
+        self.faultReason("Sorry, we were not able to find any breweries");
+      }
+      
+
+  }).error(function(e) {
+    self.faultReason("Sorry, we were not able to load the data");
+    console.log("There was an error getting brewery info")});
+
+};
+
 
 // Knockout ViewModel
 var beerMapViewModel = function() {
@@ -138,7 +155,7 @@ var beerMapViewModel = function() {
    openInfoWindow(self.brewData().breweryInfo(), self.mapMarkers()[self.brewData().breweryInfo().indexOf(data)]);
   };
 
-//setTimeout(function(){console.log(self.mapMarkers()[1]);}, 4000);
+setTimeout(function(){setBounds(self.brewData().breweryInfo());;}, 3000);
 //setTimeout(function(){openInfoWindow(self.brewData().breweryInfo(), self.mapMarkers()[3]);}, 4000);
 
 
